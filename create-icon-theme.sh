ff1d4de93da2db9be9e1d3fed783cb253c5f19c6
Hylke Bons <hylke.bons@intel.com>
2009-10-08 15:38:38 +0100

    MAke script use Scalable for 48x48 instead of Fixed

--- create-icon-theme.sh
+++ create-icon-theme.sh
@@ -38,6 +38,7 @@ echo -n "Directories=" >> index.theme
 
 DIRS=`find * -type d | grep -v git | grep -v scalable | grep "/"`
 
+
 for foo in $DIRS
 do
 	echo -n "$foo," >> index.theme
@@ -45,9 +46,13 @@ done
 
 for foo in $DIRS
 do
-	echo -en "\n\n[$foo]\nSize=`echo $foo | sed 's/\x.*//'`\nContext=`basename $foo`\nType=Fixed" >> index.theme
+	foo=`echo $foo | cut -b -5`
+	if [ "$foo" = 48x48 ] ; then
+		echo -en "\n\n[$foo]\nSize=`echo $foo | sed 's/\x.*//'`\nContext=`basename $foo`\nType=Scalable" >> index.theme
+	else
+		echo -en "\n\n[$foo]\nSize=`echo $foo | sed 's/\x.*//'`\nContext=`basename $foo`\nType=Fixed" >> index.theme
+	fi
 done
-
 
 echo "Creating Makefiles"
 
